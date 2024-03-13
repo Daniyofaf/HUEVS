7f6e3bd959c5e7528eb831f8b84c0574bc7ae0b5
daniyofaf <daniyoman1993@gmail.com>
2024-03-13 21:39:02 +0300

    Homepage Edited

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
+    # path('', homepage, name='homepage'),  # Root URL pattern
     path('', views.account_login, name="account_login"),
     path('register/', views.account_register, name="account_register"),
     path('logout/', views.account_logout, name="account_logout"),
